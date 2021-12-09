fa03dd94b99af24da195e02fc92b29caf29a6c2d
Yenifer <guajeyenifer@gmail.com>
2021-12-08 20:53:59 -0500

    Se realiza la terminación del proyecto Jest+Mock

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import * as math from './components/math.js';
 
 function App() {
   return (
@@ -23,3 +24,8 @@ function App() {
 }
 
 export default App;
+
+export const doAdd      = (a, b) => math.add(a, b);
+export const doSubtract = (a, b) => math.subtract(a, b);
+export const doMultiply = (a, b) => math.multiply(a, b);
+export const doDivide   = (a, b) => math.divide(a, b);
